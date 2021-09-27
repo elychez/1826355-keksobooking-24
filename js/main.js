25238a60c788796ea47265451014618147d97cf4
Elcins <elcins.gasanovs@gmail.com>
2021-09-27 22:50:56 +0300

    2.15. Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
-export function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomIntInclusive(min, max) {
+  if (min >= 0 && max >= min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
 
-export function getRandomArbitrary(min, max, float) {
+getRandomIntInclusive(1, 57);
+
+function getRandomArbitrary(min, max, float) {
   if (min >= 0 && max >= min) {
     const randomNumber = Math.random() * (max - min) + min;
     return randomNumber.toFixed(float);
   }
 }
 
+getRandomArbitrary(1, 77, 3);
