dd23499aeee079a7a30d40859d040d2ae58f27b1
Elcins <elcins.gasanovs@gmail.com>
2021-10-06 23:36:18 +0300

    4.2

--- js/main.js
+++ js/main.js
@@ -1,39 +1,9 @@
-const getRandomIntInclusive = function(min, max) {
-  if (min >= 0 && max >= min) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+const COORDINATES = {
+  MaxLatitude: 35.70000,
+  MinLatitude: 35.65000,
+  MaxLongitude: 139.80000,
+  MinLongitude: 139.70000,
 };
-
-const getRandomItem = function (arr) {
-  return arr[getRandomIntInclusive(0, arr.length - 1)];
-};
-
-const getRandomArbitrary = function(min, max, float) {
-  if (min >= 0 && max >= min) {
-    const randomNumber = Math.random() * (max - min) + min;
-    return randomNumber.toFixed(float);
-  }
-};
-
-getRandomArbitrary(1, 77, 3);
-
-const shuffleArray = function(data) {
-  const array = data.slice();
-  for (let itt = array.length - 1; itt > 0; itt--) {
-    const jtt = Math.floor(Math.random() * (itt + 1));
-    const temp = array[itt];
-    array[itt] = array[jtt];
-    array[jtt] = temp;
-  }
-  return array;
-};
-
-const MIN_LATITUDE = 35.65000;
-const MAX_LATITUDE = 35.70000;
-const MIN_LONGITUDE = 139.70000;
-const MAX_LONGITUDE = 139.80000;
 
 const TYPE = [
   'palace',
@@ -103,11 +73,43 @@ const DESCRIPTIONS = [
   'Рядом дорожки для бега вдоль океана',
 ];
 
+const getRandomIntInclusive = function(min, max) {
+  if (min >= 0 && max >= min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+};
+
+const getRandomItem = function (arr) {
+  return arr[getRandomIntInclusive(0, arr.length - 1)];
+};
+
+const getRandomArbitrary = function(min, max, float) {
+  if (min >= 0 && max >= min) {
+    const randomNumber = Math.random() * (max - min) + min;
+    return randomNumber.toFixed(float);
+  }
+};
+
+getRandomArbitrary(1, 77, 3);
+
+const shuffleArray = function(data) {
+  const array = data.slice();
+  for (let itt = array.length - 1; itt > 0; itt--) {
+    const jtt = Math.floor(Math.random() * (itt + 1));
+    const temp = array[itt];
+    array[itt] = array[jtt];
+    array[jtt] = temp;
+  }
+  return array;
+};
+
 const generateMokData = function () {
   const data = [];
   for (let itt = 0; itt < 10; itt++) {
-    const lat = getRandomArbitrary(MIN_LATITUDE, MAX_LATITUDE, 6);
-    const lng = getRandomArbitrary(MIN_LONGITUDE, MAX_LONGITUDE, 6);
+    const lat = getRandomArbitrary(COORDINATES.MinLatitude, COORDINATES.MaxLatitude, 6);
+    const lng = getRandomArbitrary(COORDINATES.MinLongitude, COORDINATES.MaxLongitude, 6);
     data.push({
       author: {
         avatar: `img/avatars/user${getRandomItem(PHOTO_NUMBERS)}.png`,
