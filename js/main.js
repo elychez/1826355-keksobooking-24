9797917635d013677ddbc38f38622a2cdc67184b
Elcins <elcins.gasanovs@gmail.com>
2021-10-16 14:25:21 +0300

    7.10. Отрисуй меня полностью (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
 import {generateMockData} from './mocks.js';
-
-generateMockData();
+import {renderCard} from './cards.js';
 
 const cardArray = generateMockData();
-const cardArrayElement = cardArray[0];
-console.log('cardArrayElement', cardArrayElement);
 
-const mapCanvas = document.querySelector('.map__canvas');
-const mapCard = document.querySelector('#card').content.querySelector('.popup');
-const renderCard = function (cardElement) {
-  const card = mapCard.cloneNode(true);
-  card.querySelector('.popup__title').textContent = cardElement.offer.title;
-  card.querySelector('.popup__text--address').textContent = cardElement.offer.address;
-  card.querySelector('.popup__text--price').textContent = `${cardElement.offer.price}₽/ночь`;
-  mapCanvas.appendChild(card);
-};
-
-renderCard(cardArrayElement);
+renderCard(cardArray[0]);
